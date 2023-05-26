100b0b0efabd41549bdd84a7b52a52cb440ecb08
Luis Moises Vega Aguero <37491955+luis-aguero1997@users.noreply.github.com>
2023-05-26 00:49:53 -0600

    Se agregaron las función de Javascript

---
+++ script.js
@@ -0,0 +1,42 @@
+const textArea = document.querySelector(".TxtArea");
+const mensaje = document.querySelector(".TxtMensaje");
+
+function BtnEncriptar(){
+    const TextoEncriptado = Encriptar(textArea.value);
+    mensaje.value = TextoEncriptado;
+    textArea,value = "";
+    mensaje.style.backgroundImage = "none";
+}
+
+function BtnDesncriptar(){
+    const TextoDesencriptado = Desencriptar(textArea.value);
+    mensaje.value = TextoDesencriptado;
+    textArea,value = "";
+    mensaje.style.backgroundImage = "none";
+}
+
+
+function Encriptar(TMensaje){
+    let matrizCodigo = [["e", "enter"], ["i", "imes",], ["o", "ober"], ["a", "ai"], ["u", "ufat"]];
+    TMensaje = TMensaje.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(TMensaje.includes(matrizCodigo[i][0])){
+            TMensaje = TMensaje.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    } 
+    return TMensaje;
+}
+
+function Desencriptar(TMensaje){
+    let matrizCodigo = [["e", "enter"], ["i", "imes",], ["o", "ober"], ["a", "ai"], ["u", "ufat"]];
+    TMensaje = TMensaje.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(TMensaje.includes(matrizCodigo[i][1])){
+            TMensaje = TMensaje.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    } 
+    return TMensaje;
+}
+
